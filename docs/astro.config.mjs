5c17ec3a5949064d52c2f632d2fa7d061404a368
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-08-19 18:16:01 +0800

    docs: test out some new stuff

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,25 +2,33 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+     title: 'rovr docs',
+     social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/NSPC911/rovr' }],
+     customCss: [
+       "./src/styles/custom.css"
+     ],
+     sidebar: [
+      { label: 'Overview', slug: 'overview' },
+      {
+        label: 'Get Started',
+        items: [
+          { label: 'Installation', slug: 'get-started/installation' },
+          { label: 'Tutorial', slug: 'get-started/tutorial'},
+          { label: 'Protocols', slug: 'get-started/protocol'}
+        ]
+      },
+      {
+        label: 'Configuration',
+        items: [
+         { label: 'Configuration', slug: 'configuration/configuration' },
+         { label: 'Themeing', slug: 'configuration/themeing' }
+        ]
+      },
+      { label: 'Config Schema', slug: 'schema' }
+     ],
+    }),
+  ],
 });

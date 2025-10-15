82f49cec2651d63d84c25ba04aa7ac058ae15530
NSPBot911 <176916861+NSPBot911@users.noreply.github.com>
2025-10-15 16:40:42 +0000

    style: auto-format with ruff

--- src/rovr/functions/config.py
+++ src/rovr/functions/config.py
@@ -176,7 +176,7 @@ def schema_dump(doc_path: str, exception: ValidationError, config_content: str) 
             case "enum":
                 error_msg = f"Provided value '{exception.instance}' is not inside allowlist of {exception.validator_value}"
             case "minimum":
-                error_msg = f"Value for [bright_cyan]{".".join(exception.relative_path)}[/] must be < {exception.validator_value} (cannot be {exception.instance})"
+                error_msg = f"Value for [bright_cyan]{'.'.join(exception.relative_path)}[/] must be < {exception.validator_value} (cannot be {exception.instance})"
             case _:
                 print(exception.validator)
                 error_msg = exception.message

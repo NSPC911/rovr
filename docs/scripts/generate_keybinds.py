05ef77aee1cee4d8aeb689b81b1c3647cbe01641
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-10-24 00:27:06 +0800

    update docs

--- docs/scripts/generate_keybinds.py
+++ docs/scripts/generate_keybinds.py
@@ -20,9 +20,9 @@ this page provides a comprehensive list of the default keybindings in `rovr`. yo
 | action | default hotkey | description |
 | ------ | -------------- | ----------- |"""
 try:
-    with open("src/rovr/config/config.toml", "r") as file:
+    with open("src/rovr/config/config.toml", "r", encoding="utf-8") as file:
         binds: dict = toml.load(file)["keybinds"]
-    with open("src/rovr/config/schema.json", "r") as file:
+    with open("src/rovr/config/schema.json", "r", encoding="utf-8") as file:
         schema: dict = ujson.load(file)["properties"]["keybinds"]["properties"]
     for key, values in schema.items():
         to_add = "\n| "
@@ -34,18 +34,26 @@ try:
         to_add += values["display_name"]
         to_add += " |"
         page += to_add
-    with open("docs/src/content/docs/reference/keybindings.mdx", "w") as file:
+    with open(
+        "docs/src/content/docs/reference/keybindings.mdx", "w", encoding="utf-8"
+    ) as file:
         file.write(page)
     # attempt to format it
     if which("prettier"):
-        run(["prettier", "--write", "docs/src/content/docs/reference/keybindings.mdx"])
+        run(
+            ["prettier", "--write", "docs/src/content/docs/reference/keybindings.mdx"],
+            shell=True,
+        )
     elif which("npx"):
-        run([
-            "npx",
-            "prettier",
-            "--write",
-            "docs/src/content/docs/reference/keybindings.mdx",
-        ])
+        run(
+            [
+                "npx",
+                "prettier",
+                "--write",
+                "docs/src/content/docs/reference/keybindings.mdx",
+            ],
+            shell=True,
+        )
     else:
         pprint(
             "[red][blue]prettier[/] and [blue]npx[/] are not available on PATH, and hence the generated files cannot be formatted."
--- docs/scripts/generate_schema.py
+++ docs/scripts/generate_schema.py
@@ -1,24 +1,64 @@
+from shutil import which
+from subprocess import run
+from time import perf_counter
+
+from humanize import naturaldelta
 from json_schema_for_humans.generate import generate_from_filename
 from json_schema_for_humans.generation_configuration import GenerationConfiguration
+from rich.console import Console
 
-config = GenerationConfiguration()
-if config.template_md_options is not None:
-    config.template_md_options["properties_table_columns"] = [
-        "Property",
-        "Type",
-        "Title/Description",
-    ]
-config.template_name = "md"
-config.with_footer = False
-generate_from_filename(
-    "src/rovr/config/schema.json",
-    "docs/src/content/docs/reference/schema.mdx",
-    config=config,
-)
-with open("docs/src/content/docs/reference/schema.mdx", "r") as schema_file:
-    content = schema_file.read()
-with open("docs/src/content/docs/reference/schema.mdx", "w") as schema_file:
-    schema_file.write(
-        """---\ntitle: schema for humans\ndescription: config schema humanified\n---"""
-        + content[13:].replace("| - ", "|   ").replace("| + ", "|   ")
+start_time = perf_counter()
+pprint = Console().print
+
+try:
+    config = GenerationConfiguration()
+    if config.template_md_options is not None:
+        config.template_md_options["properties_table_columns"] = [
+            "Property",
+            "Type",
+            "Title/Description",
+        ]
+    config.template_name = "md"
+    config.with_footer = False
+    generate_from_filename(
+        "src/rovr/config/schema.json",
+        "docs/src/content/docs/reference/schema.mdx",
+        config=config,
     )
+    with open(
+        "docs/src/content/docs/reference/schema.mdx", "r", encoding="utf-8"
+    ) as schema_file:
+        content = schema_file.read()
+    with open(
+        "docs/src/content/docs/reference/schema.mdx", "w", encoding="utf-8"
+    ) as schema_file:
+        schema_file.write(
+            """---\ntitle: schema for humans\ndescription: config schema humanified\n---"""
+            + content[13:].replace("| - ", "|   ").replace("| + ", "|   ")
+        )
+    if which("prettier"):
+        run(
+            ["prettier", "--write", "docs/src/content/docs/reference/schema.mdx"],
+            shell=True,
+        )
+    elif which("npm"):
+        run(
+            [
+                "npx",
+                "prettier",
+                "--write",
+                "docs/src/content/docs/reference/schema.mdx",
+            ],
+            shell=True,
+        )
+    else:
+        pprint(
+            "[red][blue]prettier[/] and [blue]npx[/] are not available on PATH, and hence the generated files cannot be formatted."
+        )
+        exit(1)
+    pprint(
+        f"[green]Generated it in {naturaldelta(perf_counter() - start_time, minimum_unit='microseconds')}"
+    )
+except FileNotFoundError:
+    pprint("[red]Do not run manually with python! Run [blue]poe gen-keys[/][/]")
+    exit(1)

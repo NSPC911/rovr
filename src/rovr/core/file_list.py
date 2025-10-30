4825c4fad5b74164cf61a203cd1cc7f3d0df9c17
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-10-30 12:40:28 +0800

    peak

--- src/rovr/core/file_list.py
+++ src/rovr/core/file_list.py
@@ -9,6 +9,7 @@ from rich.style import Style
 from textual import events, on, work
 from textual.binding import BindingType
 from textual.css.query import NoMatches
+from textual.geometry import Region
 from textual.strip import Strip
 from textual.widgets import Button, Input, OptionList, SelectionList
 from textual.widgets.option_list import Option, OptionDoesNotExist
@@ -797,6 +798,54 @@ class FileList(SelectionList, inherit_bindings=False):
         else:
             utils.set_scuffed_subtitle(
                 self.parent, "SELECT", f"{len(self.selected)}/{len(self.options)}"
+            )
+
+    def scroll_to_highlight(
+        self, top: bool = False, scrolloff: int = config["interface"]["scrolloff"]
+    ) -> None:
+        """Scroll to the highlighted option.
+
+        Args:
+            top: Ensure highlighted option is at the top of the widget.
+            scrolloff: Minimum number of lines to keep visible above/below the highlighted option.
+                If scrolloff is larger than half the screen height, the cursor will be centered.
+        """
+        highlighted = self.highlighted
+        if type(highlighted) is not int or not self.is_mounted:
+            return
+
+        self._update_lines()
+
+        try:
+            y = self._index_to_line[highlighted]
+        except KeyError:
+            return
+        height = self._heights[highlighted]
+
+        # --peak-monkey-patching #
+        scrollable_height = self.scrollable_content_region.height
+
+        # yazi like
+        if scrolloff > scrollable_height / 2:
+            super().scroll_to_region(
+                Region(0, y, self.scrollable_content_region.width, height),
+                force=True,
+                animate=False,
+                center=True,
+                immediate=True,
+            )
+        else:
+            adjusted_y = max(0, y - scrolloff)
+            adjusted_height = height + scrolloff * 2
+
+            super().scroll_to_region(
+                Region(
+                    0, adjusted_y, self.scrollable_content_region.width, adjusted_height
+                ),
+                force=True,
+                animate=False,
+                top=top,
+                immediate=True,
             )
 
 

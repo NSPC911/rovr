09de59c8283ae8c554af6a7a9006f0bf3e794ccf
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-11-10 17:08:36 +0800

    fix(copy_path): make use of dir entry

--- src/rovr/action_buttons/copy_path.py
+++ src/rovr/action_buttons/copy_path.py
@@ -1,5 +1,6 @@
-from textual.widgets import Button
+from textual.widgets import Button, SelectionList
 
+from rovr.classes.textual_options import FileListSelectionWidget
 from rovr.functions.icons import get_icon
 from rovr.variables.constants import config
 
@@ -24,17 +25,11 @@ class PathCopyButton(Button):
         """Copy selected files to the clipboard"""
         if self.disabled:
             return
-        selected_files = await self.app.query_one("#file_list").get_selected_objects()
-        if len(selected_files) == 1:
-            self.app.copy_to_clipboard(selected_files[0])
-            self.notify("Copied!", title="Copy Path", severity="information")
-        elif len(selected_files) > 1:
-            self.notify(
-                "Exactly one must be selected.",
-                title="Copy Path",
-                severity="information",
-            )
-        else:
+        highlighted: FileListSelectionWidget | None = await self.app.query_one("#file_list", SelectionList).highlighted_option
+        if highlighted is None or not hasattr(highlighted, "dir_entry"):
             self.notify(
                 "No items were selected.", title="Copy Path", severity="information"
             )
+        else:
+            self.app.copy_to_clipboard(highlighted.dir_entry.path)
+            self.notify("Copied!", title="Copy Path", severity="information")

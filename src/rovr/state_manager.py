841db91f1a93a8485d9416f518f52b0808d95b88
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-11-11 22:58:28 +0800

    add a pseudo locking mechanism

--- src/rovr/state_manager.py
+++ src/rovr/state_manager.py
@@ -1,6 +1,6 @@
 from contextlib import suppress
 from os import path
-from typing import TypedDict
+from typing import Literal, TypedDict
 
 import toml
 from textual.css.query import NoMatches
@@ -40,6 +40,7 @@ class StateManager(Widget):
         self._is_loading = False
         self._load_state()
         self._skip_save = False
+        self._locked_by: Literal["PinnedSidebar", "PreviewSidebar", "Footer", "menuwrapper", None] = None
 
     def _load_state(self) -> None:
         self._is_loading = True
@@ -101,46 +102,54 @@ class StateManager(Widget):
     def watch_pinned_sidebar_visible(self, visible: bool) -> None:
         if self._is_loading:
             return
+        self._locked_by = "PinnedSidebar"
         with suppress(NoMatches):
             pinned_sidebar = self.app.query_one("#pinned_sidebar_container")
             if visible:
                 pinned_sidebar.remove_class("hide")
             else:
                 pinned_sidebar.add_class("hide")
-        self._save_state()
+        if self._locked_by == "PinnedSidebar":
+            self._save_state()
 
     def watch_preview_sidebar_visible(self, visible: bool) -> None:
         if self._is_loading:
             return
+        self._locked_by = "PreviewSidebar"
         with suppress(NoMatches):
             preview_sidebar = self.app.query_one("PreviewContainer")
             if visible:
                 preview_sidebar.remove_class("hide")
             else:
                 preview_sidebar.add_class("hide")
-        self._save_state()
+        if self._locked_by == "PreviewSidebar":
+            self._save_state()
 
     def watch_footer_visible(self, visible: bool) -> None:
         if self._is_loading:
             return
+        self._locked_by = "Footer"
         with suppress(NoMatches):
             footer = self.app.query_one("#footer")
             if visible:
                 footer.remove_class("hide")
             else:
                 footer.add_class("hide")
-        self._save_state()
+        if self._locked_by == "Footer":
+            self._save_state()
 
     def watch_menuwrapper_visible(self, visible: bool) -> None:
         if self._is_loading:
             return
+        self._locked_by = "menuwrapper"
         with suppress(NoMatches):
             menuwrapper = self.app.query_one("#menuwrapper")
             if visible:
                 menuwrapper.remove_class("hide")
             else:
                 menuwrapper.add_class("hide")
-        self._save_state()
+        if self._locked_by == "menuwrapper":
+            self._save_state()
 
     def toggle_pinned_sidebar(self) -> None:
         self.pinned_sidebar_visible = not self.pinned_sidebar_visible

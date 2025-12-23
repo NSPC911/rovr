1b3984a83c10b1bb1950d6c2af3c83790d215cc3
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-12-23 18:51:46 +0800

    puremagic: fix issues

--- src/rovr/monkey_patches/puremagic_patch.py
+++ src/rovr/monkey_patches/puremagic_patch.py
@@ -4,35 +4,50 @@ from binascii import unhexlify
 import puremagic
 import ujson
 
+try:
+    if puremagic.__version__ != "1.30":
+        raise RuntimeError(
+            f"Expected puremagic version 1.30, but found {puremagic.__version__}"
+        )
+except RuntimeError:
+    from rich.console import Console
 
-def _magic_data(
-    filename: os.PathLike | str = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "magic_data.json"
-    ),
-) -> tuple[
+    Console().print_exception()
+    raise SystemExit(1)
+
+
+def _magic_data() -> tuple[
     list[puremagic.PureMagic],
     list[puremagic.PureMagic],
     list[puremagic.PureMagic],
     dict[bytes, list[puremagic.PureMagic]],
 ]:
     """Read the magic file"""  # noqa: DOC201
+    filename = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "magic_data.json"
+    )
     with open(filename, encoding="utf-8") as f:
         data = ujson.load(f)
     headers = sorted(
-        (puremagic._create_puremagic(x) for x in data["headers"]),
+        (puremagic.main._create_puremagic(x) for x in data["headers"]),
         key=lambda x: x.byte_match,
     )
     footers = sorted(
-        (puremagic._create_puremagic(x) for x in data["footers"]),
+        (puremagic.main._create_puremagic(x) for x in data["footers"]),
         key=lambda x: x.byte_match,
     )
-    extensions = [puremagic._create_puremagic(x) for x in data["extension_only"]]
+    extensions = [puremagic.main._create_puremagic(x) for x in data["extension_only"]]
     multi_part_extensions = {}
     for file_match, option_list in data["multi-part"].items():
         multi_part_extensions[unhexlify(file_match.encode("ascii"))] = [
-            puremagic._create_puremagic(x) for x in option_list
+            puremagic.main._create_puremagic(x) for x in option_list
         ]
     return headers, footers, extensions, multi_part_extensions
 
 
-puremagic._magic_data = _magic_data
+(
+    puremagic.main.magic_header_array,
+    puremagic.main.magic_footer_array,
+    puremagic.main.extension_only_array,
+    puremagic.main.multi_part_dict,
+) = _magic_data()

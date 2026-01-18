505f329273d96bf15c90d8b1977eb26d62967a32
NSPBot911 <176916861+NSPBot911@users.noreply.github.com>
2026-01-18 01:44:51 +0000

    style: auto-format with ruff
    [skip ci]
    
    NSPC911 forgot to run ruff format
    
    skip-checks: true

--- src/rovr/action_buttons/copy_button.py
+++ src/rovr/action_buttons/copy_button.py
@@ -179,9 +179,7 @@ class CopyPanelOptions(PopupOptionList):
         super().__init__()
         self.do_adjust: bool = False
 
-    def on_mount(
-        self, event: events.Mount
-    ) -> None:  # ty: ignore[invalid-method-override]
+    def on_mount(self, event: events.Mount) -> None:  # ty: ignore[invalid-method-override]
         # calling super()._on_mount is useless, and super().mount()
         # doesnt do anything significant, hence ty ignore
         self.button: CopyButton = self.app.query_one(CopyButton)

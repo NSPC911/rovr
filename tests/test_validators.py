02d37b63a7a17a891e40db68fdd3d42bfc9bee20
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-02-27 19:01:30 +0800

    validators test

---
+++ tests/test_validators.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from rovr.classes.textual_validators import (
+    IsValidFilePath,
+    PathNoLongerExists,
+)
+
+
+def test_is_valid_file_path() -> None:
+    validator = IsValidFilePath()
+    assert validator.validate("valid_filename.txt").is_valid
+    assert not validator.validate("invalid\u0000filename.txt").is_valid
+
+
+def test_path_no_longer_exists(tmp_path: Path) -> None:
+    os.chdir(tmp_path)
+    validator = PathNoLongerExists()
+    existing_file = tmp_path / "existing_file.txt"
+    existing_file.touch()
+    assert not validator.validate("existing_file.txt").is_valid
+    if os.name == "nt":
+        assert not validator.validate("EXISTING_FILE.txt").is_valid
+    assert validator.validate("non_existent_file.txt").is_valid

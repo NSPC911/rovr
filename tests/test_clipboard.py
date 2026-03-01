60e0f14bfc34337da3cf9783bb4ac7b7653e45b8
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-03-01 16:28:38 +0800

    please tell me  i fixed it

--- tests/test_clipboard.py
+++ tests/test_clipboard.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from textual.style import Style
 from textual.worker import Worker
 
 from rovr.app import Application
@@ -30,6 +31,11 @@ async def test_delete_from_clipboard(tmp_path: Path) -> None:
 
 @pytest.mark.asyncio
 async def test_dimming(tmp_path: Path) -> None:
+    def check_dim(style: str | Style) -> bool:
+        if isinstance(style, str):
+            style = Style.parse(style)
+        return style.dim or False
+
     file = tmp_path / "file.txt"
     file.touch()
     app = Application(tmp_path.as_posix())
@@ -39,25 +45,20 @@ async def test_dimming(tmp_path: Path) -> None:
         await pilot.click("CutButton")
         await pilot.pause()
         # get first option's dimness
-        assert app.screen.get_style_at(
-            app.file_list.region.top_right.x - app.file_list.region.size.width,
-            app.file_list.region.top_right.y,
-        ).dim
+        assert check_dim(app.file_list.options[0]._prompt.spans[-1].style)
         clipboard.highlighted = 0
         await pilot.pause()
         clipboard.action_select()
         await pilot.pause(1)
         # once unselected, the dimness should be gone
-        assert not app.screen.get_style_at(
-            app.file_list.region.top_right.x - app.file_list.region.size.width,
-            app.file_list.region.top_right.y,
-        ).dim
+        assert not check_dim(app.file_list.options[0]._prompt.spans[-1].style)
         clipboard.action_select()
         await pilot.pause(1)
         await pilot.click("CopyButton")
         await pilot.pause()
         # copying should not cause dimness
-        assert not app.screen.get_style_at(
-            app.file_list.region.top_right.x - app.file_list.region.size.width,
-            app.file_list.region.top_right.y,
-        ).dim
+        assert not check_dim(app.file_list.options[0]._prompt.spans[-1].style)
+        # then retry cutting, should cause dimness again
+        await pilot.click("CutButton")
+        await pilot.pause()
+        assert check_dim(app.file_list.options[0]._prompt.spans[-1].style)

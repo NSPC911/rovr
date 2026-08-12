244c382ff9ba48cf5fa95f89d9d30ddb26c9c829
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-08-12 17:41:15 +0800

    update docstring

--- src/rovr/classes/session_manager.py
+++ src/rovr/classes/session_manager.py
@@ -27,7 +27,7 @@ class SessionManager:
                                  `settings.history_size` entries (least-recently-used
                                  eviction). If a directory is not in the dictionary, the
                                  default is 0. Use `remember_highlight` to write to it.
-        selectMode (bool): Whether select mode is enabled for that directory.
+        selectMode (False | "implicit" | "explicit"): Whether select mode is enabled for that directory.
         selectedItems (list[SessionOptionDict]): A list of selected items within the
                                                                       current directory
         search (str): The current search string.

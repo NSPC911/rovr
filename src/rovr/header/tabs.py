88cc886c05dc71af33822216632d27bf5f983bf3
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-09-07 15:23:42 +0800

    refactor(tabs): nicer tabs ergonomics ig

--- src/rovr/header/tabs.py
+++ src/rovr/header/tabs.py
@@ -92,9 +92,35 @@ class TablineTab(Tab):
 
 
 class Tabline(Tabs):
+    @property
+    def _potentially_active_tabs(self) -> list[TablineTab]:
+        """List of all tabs that could be active.
+
+        This list is comprised of all tabs that are shown and enabled,
+        plus the active tab in case it is disabled.
+        """
+        return [
+            tab
+            for tab in self.query("#tabs-list > TablineTab").results(TablineTab)
+            if ((not tab.disabled or tab is self.active_tab) and tab.display)
+        ]
+
+    @property
+    def active_tab(self) -> TablineTab | None:
+        """The currently active tab, or None if there are no active tabs."""
+        try:
+            return self.query_one("#tabs-list Tab.-active", TablineTab)
+        except NoMatches:
+            return None
+
+    @property
+    def tabs(self) -> list[TablineTab]:
+        # better ergonomics i guess
+        return self._potentially_active_tabs
+
     def action_cycle_tab(self, offset: int) -> None:
         """Cycle and activate a tab at an offset relative from the current tab."""
-        if not offset or not (tabs := self._potentially_active_tabs):
+        if not offset or not (tabs := self.tabs):
             return
 
         if self.active_tab is None:
@@ -105,7 +131,7 @@ class Tabline(Tabs):
 
     def action_activate_tab(self, index: int) -> None:
         """Activate a tab by 0-base index."""
-        if not (tabs := self._potentially_active_tabs):
+        if not (tabs := self.tabs):
             return
         if index < 0 or index > len(tabs) - 1:
             return

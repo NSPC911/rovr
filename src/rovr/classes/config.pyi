c6d36486879324586fbba9ddf9f7ef170b0db308
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-09-18 15:55:54 +0800

    restrict a bit

--- src/rovr/classes/config.pyi
+++ src/rovr/classes/config.pyi
@@ -385,25 +385,27 @@ _ROVR_CONFIG_SETTINGS_OPENERS_GROUPS_ADDITIONALPROPERTIES_ITEM_ONEOF1_SHELL_DEFA
 r""" Default value of the field path 'Rovr Config settings openers groups additionalProperties item oneof1 shell' """
 
 _ROVR_CONFIG_SETTINGS_PREVIEW_RULES_DEFAULT = {
-    "text/.*": "text",
-    "application/(json|javascript|xml|raml\\+yaml)": "text",
-    "application/x-(yaml|script|pem-file|subrip|typescript)": "text",
-    "application/(mbox|ndjson|wine-extension-ini)": "text",
-    "image/svg\\+xml": "resvg",
-    "image/(avif|hei.|jxl)": "image",
-    "image/.*": "image",
-    "application/pdf": "pdf",
-    "application/(zip|gzip|zstd|bzip2|vnd\\.rar)": "archive",
-    "application/x-(xz|x-tar|x-gzip|x-bzip2|x-xz|x-rar|x-rar-compressed|x-7z-compressed)": "archive",
-    "application/(rar|7z.*|tar|xz|bzip.*|lzma|compress|archive|cpio|arj|xar|ms-cab.*)": "archive",
-    "application/(iso9660-image|qemu-disk|ms-wim|apple-diskimage)": "archive",
-    "application/virtualbox-(vhd|vhdx)": "archive",
     "application/(debian.*-package|redhat-package-manager|rpm|android\\.package-archive)": "archive",
-    "inode/directory": "folder",
-    "font/.*": "font",
+    "application/(iso9660-image|qemu-disk|ms-wim|apple-diskimage)": "archive",
+    "application/(rar|7z.*|tar|xz|bzip.*|lzma|compress|archive|cpio|arj|xar|ms-cab.*)": "archive",
+    "application/(zip|zstd|bzip2|vnd\\.rar)": "archive",
+    "application/java-archive": "archive",
+    "application/virtualbox-(vhd|vhdx)": "archive",
+    "application/x-(xz|tar|bzip2|xz|rar|rar-compressed|7z-compressed|zstd-compressed-tar)": "archive",
+    "application/(json|javascript|xml|raml\\+yaml)": "text",
+    "application/(mbox|ndjson|wine-extension-ini)": "text",
+    "application/x-(yaml|script|pem-file|subrip|typescript)": "text",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "remime",
     "application/ms-opentype": "font",
+    "application/pdf": "pdf",
+    "image/svg\\+xml": "resvg",
+    "inode/directory": "folder",
     "application/font-.*": "font",
     "application/x-font-.*": "font",
+    "image/.*": "image",
+    "text/.*": "text",
+    "font/.*": "font",
+    "application/(x-)?gzip": {"default": "archive", "extensions": {"svgz": "resvg"}},
 }
 r""" Default value of the field path 'Rovr Config settings preview_rules' """
 
@@ -1564,16 +1566,21 @@ class _RovrConfigSettings(TypedDict, total=False):
       application/(json|javascript|xml|raml\+yaml): text
       application/(mbox|ndjson|wine-extension-ini): text
       application/(rar|7z.*|tar|xz|bzip.*|lzma|compress|archive|cpio|arj|xar|ms-cab.*): archive
-      application/(zip|gzip|zstd|bzip2|vnd\.rar): archive
+      application/(x-)?gzip:
+        default: archive
+        extensions:
+          svgz: resvg
+      application/(zip|zstd|bzip2|vnd\.rar): archive
       application/font-.*: font
+      application/java-archive: archive
       application/ms-opentype: font
       application/pdf: pdf
       application/virtualbox-(vhd|vhdx): archive
-      application/x-(xz|x-tar|x-gzip|x-bzip2|x-xz|x-rar|x-rar-compressed|x-7z-compressed): archive
+      application/vnd.openxmlformats-officedocument.wordprocessingml.document: remime
+      application/x-(xz|tar|bzip2|xz|rar|rar-compressed|7z-compressed|zstd-compressed-tar): archive
       application/x-(yaml|script|pem-file|subrip|typescript): text
       application/x-font-.*: font
       font/.*: font
-      image/(avif|hei.|jxl): image
       image/.*: image
       image/svg\+xml: resvg
       inode/directory: folder
